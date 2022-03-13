0b0262f5580a248a4d5f09475bb3ab509443248f
zeynelok <zeynel_2406@hotmail.com>
2022-03-13 12:39:59 +0300

    Entity mapping started

---
+++ Data/ProjectTracker.Data.EntityFramework/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectTracker.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTracker.Data.EntityFramework.Configurations
+{
+    internal class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+           
+
+        }
+    }
+}
